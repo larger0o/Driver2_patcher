6e8b472360ffe5adc023e41be0df553f412151b8
larger0o <largeroliker0o@gmail.com>
2018-01-13 00:28:42 +0100

    Añadidas comprobaciones de fichero y mejorada un poco la interfaz

--- Driver2_patcher/Form1.Designer.cs
+++ Driver2_patcher/Form1.Designer.cs
@@ -28,60 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnCargarImagen = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblFichero = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.btnParchear = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblDetectado = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.ShowReadOnly = true;
+            this.openFileDialog1.Filter = "Imágenes ISO| *.iso|Imágenes bin/cue|*bin";
             this.openFileDialog1.Title = "Selecciona la imagen del juego";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
             // btnCargarImagen
             // 
-            this.btnCargarImagen.Location = new System.Drawing.Point(25, 12);
+            this.btnCargarImagen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCargarImagen.Location = new System.Drawing.Point(12, 12);
             this.btnCargarImagen.Name = "btnCargarImagen";
             this.btnCargarImagen.Size = new System.Drawing.Size(75, 23);
             this.btnCargarImagen.TabIndex = 0;
             this.btnCargarImagen.Text = "Examinar";
             this.btnCargarImagen.UseVisualStyleBackColor = true;
+            this.btnCargarImagen.Click += new System.EventHandler(this.btnCargarImagen_Click);
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1,
-            this.toolStripProgressBar1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 394);
+            this.lblFichero,
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3,
+            this.lblDetectado});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 384);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(796, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(470, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
-            // toolStripStatusLabel1
+            // lblFichero
             // 
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
-            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.lblFichero.Name = "lblFichero";
+            this.lblFichero.Size = new System.Drawing.Size(0, 17);
             // 
             // toolStripProgressBar1
             // 
             this.toolStripProgressBar1.Name = "toolStripProgressBar1";
             this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Visible = false;
+            // 
+            // btnParchear
+            // 
+            this.btnParchear.Enabled = false;
+            this.btnParchear.Location = new System.Drawing.Point(357, 12);
+            this.btnParchear.Name = "btnParchear";
+            this.btnParchear.Size = new System.Drawing.Size(101, 57);
+            this.btnParchear.TabIndex = 2;
+            this.btnParchear.Text = "Parchear";
+            this.btnParchear.UseVisualStyleBackColor = true;
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(163, 17);
+            this.toolStripStatusLabel2.Spring = true;
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(163, 17);
+            this.toolStripStatusLabel3.Spring = true;
+            // 
+            // lblDetectado
+            // 
+            this.lblDetectado.Name = "lblDetectado";
+            this.lblDetectado.Size = new System.Drawing.Size(0, 17);
+            // 
+            // label1
+            // 
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(-3, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(473, 391);
+            this.label1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(796, 416);
+            this.ClientSize = new System.Drawing.Size(470, 406);
+            this.Controls.Add(this.btnParchear);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.btnCargarImagen);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Driver 2 - Parcheador";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -94,8 +144,13 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btnCargarImagen;
         private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lblFichero;
         private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button btnParchear;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel lblDetectado;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Driver2_patcher/Form1.cs
+++ Driver2_patcher/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Driver2_patcher
 {
@@ -16,5 +17,80 @@ namespace Driver2_patcher
         {
             InitializeComponent();
         }
+
+        private void btnCargarImagen_Click(object sender, EventArgs e)
+        {
+            btnParchear.Enabled = false;
+            openFileDialog1.ShowDialog();
+
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string fichero = openFileDialog1.FileName;
+            if (fichero != null)
+            {
+                // Comprobamos que el fichero es parcheable buscando las cadenas a editar
+                CargarCompatibilidad(fichero);
+
+            }
+        }
+        private void CargarCompatibilidad(string fichero)
+        {
+            string ValorEU, ValorUS, refEU, refUS;
+            int PosicionEU, PosicionUS;
+
+            //SLES_02994
+            refEU = "SLES_029.9";
+            ValorEU = "SLES_129.9";
+            PosicionEU = 154503145;
+
+            refUS = "SLUS_011.61";
+            ValorUS = "SLUS_013.18";            
+            PosicionUS = 155667209;
+
+            try
+            {
+                using (FileStream fs = File.OpenRead(fichero))
+                {
+                    lblFichero.Text = openFileDialog1.SafeFileName + " cargado";
+                    fs.Seek(PosicionEU, SeekOrigin.Begin);
+                    byte[] b = new byte[11];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+                    fs.Read(b, 0, 11);
+                    if (temp.GetString(b).Contains(refEU))
+                    {
+                        lblDetectado.Text = "Versión PAL detectada";
+                        btnParchear.Enabled = true;
+                    }
+                    else
+                    {
+                        fs.Seek(PosicionUS, SeekOrigin.Begin);
+                        fs.Read(b, 0, 11);
+                        if (temp.GetString(b).Contains(refUS))
+                        {
+                            lblDetectado.Text = "Versión NTSC detectada";
+                            btnParchear.Enabled = true;
+                        }
+                        else
+                            MessageBox.Show("El archivo proporcionado no es una imagen válida", "Imagen inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            } catch (IOException e)
+            {
+                MessageBox.Show("Error al acceder al archivo. Otra aplicación la está utilizando.", "Error al leer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("Error al acceder al archivo.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+
+
+
+        }
+
     }
 }
